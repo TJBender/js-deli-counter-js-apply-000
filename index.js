3858c6054f1ce5ed10c7cc8eda4e19485d21a595
Tiana Bender <tiana.bender01@gmail.com>
2019-08-17 21:59:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
 
 function takeANumber(line, name) {
   line.push(name);
-  return "Welcome, " + name + " You are number " + line.length + " in line.";
+  return "Welcome, " + name + ". You are number " + line.length + " in line.";
 }
 
 
